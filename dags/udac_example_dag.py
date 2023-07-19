40afe43e888d32bf4577e90ef933d9265d97ab72
Reginald <bryantre@ke.ibm.com>
2023-07-19 14:28:51 +0300

    updated final dag and sql queries.

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -1,65 +1,152 @@
 from datetime import datetime, timedelta
+import pendulum
 import os
-from airflow import DAG
+from airflow.decorators import dag
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
-                                LoadDimensionOperator, DataQualityOperator)
-from helpers import SqlQueries
-
-# AWS_KEY = os.environ.get('AWS_KEY')
-# AWS_SECRET = os.environ.get('AWS_SECRET')
+from operators.stage_redshift import StageToRedshiftOperator
+from operators.load_fact import LoadFactOperator
+from operators.load_dimension import LoadDimensionOperator
+from operators.data_quality import DataQualityOperator
+from helpers.sql_queries import SqlQueries
+from airflow.operators.postgres_operator import PostgresOperator
 
 default_args = {
-    'owner': 'udacity',
-    'start_date': datetime(2019, 1, 12),
+    'owner': 'Reginald',
+    'depends_on_past': False, # The DAG does not have dependencies on past runs
+    'start_date': pendulum.now(), 
+    'retries': 3, # On failure, the task are retried 3 times
+    'retry_delay': timedelta(minutes=5), # Retries happen every 5 minutes
+    'catchup': False, # Catchup is turned off
+    'email_on_retry': False # Do not email on retry
 }
 
-dag = DAG('udac_example_dag',
-          default_args=default_args,
-          description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *'
-        )
-
-start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
-
-stage_events_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_events',
-    dag=dag
+@dag(
+    default_args=default_args,
+    description='Load and transform data in Redshift with Airflow',
+    schedule_interval='0 * * * *'
 )
+def final_project():
 
-stage_songs_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_songs',
-    dag=dag
-)
+    start_operator = DummyOperator(task_id='Begin_execution')
+    end_operator = DummyOperator(task_id='End_execution')
 
-load_songplays_table = LoadFactOperator(
-    task_id='Load_songplays_fact_table',
-    dag=dag
-)
 
-load_user_dimension_table = LoadDimensionOperator(
-    task_id='Load_user_dim_table',
-    dag=dag
-)
+    stage_events_to_redshift = StageToRedshiftOperator(
+        task_id='Stage_events',
+        table="log_data",
+        redshift_conn_id="redshift",
+        ARN="arn:aws:iam::101621241983:role/dwhRole", 
+        aws_credentials_id="aws_credentials",
+        s3_bucket="udacity-dend",
+        s3_key="log_data",
+        s3_location="s3://udacity-dend/log_data",
+        json_format="s3://udacity-dend/log_json_path.json",
+        sql_create=SqlQueries.staging_events_table_create
+    )
 
-load_song_dimension_table = LoadDimensionOperator(
-    task_id='Load_song_dim_table',
-    dag=dag
-)
+    stage_songs_to_redshift = StageToRedshiftOperator(
+        task_id='Stage_songs',
+        table="song_data",
+        redshift_conn_id="redshift",
+        ARN="arn:aws:iam::101621241983:role/dwhRole", 
+        aws_credentials_id="aws_credentials",
+        s3_bucket="udacity-dend",
+        s3_key="song_data/A/A",
+        s3_location="s3://udacity-dend/song_data/A/A",
+        json_format = "auto",
+        sql_create=SqlQueries.staging_songs_table_create
+    )
 
-load_artist_dimension_table = LoadDimensionOperator(
-    task_id='Load_artist_dim_table',
-    dag=dag
-)
 
-load_time_dimension_table = LoadDimensionOperator(
-    task_id='Load_time_dim_table',
-    dag=dag
-)
+    load_songplays_table = LoadFactOperator(
+        task_id='Load_songplays_fact_table',
+        redshift_conn_id="redshift",
+        params={
+            'table': "factSongPlays"
+        },
+        sql_create=SqlQueries.songplay_table_create,
+        sql_insert=SqlQueries.songplay_table_insert
+    )
 
-run_quality_checks = DataQualityOperator(
-    task_id='Run_data_quality_checks',
-    dag=dag
-)
+    load_user_dimension_table = LoadDimensionOperator(
+        task_id='Load_user_dim_table',
+        redshift_conn_id="redshift",
+        params={
+            'table': "dimUsers"
+        },
+        sql_delete=SqlQueries.user_table_delete,
+        sql_create=SqlQueries.user_table_create,
+        sql_insert=SqlQueries.user_table_insert
+    )
 
-end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+    load_song_dimension_table = LoadDimensionOperator(
+        task_id='Load_song_dim_table',
+        redshift_conn_id="redshift",
+        params={
+            'table': "dimSongs"
+        },
+        sql_delete=SqlQueries.song_table_delete,
+        sql_create=SqlQueries.song_table_create,
+        sql_insert=SqlQueries.song_table_insert
+    )
+
+    load_artist_dimension_table = LoadDimensionOperator(
+        task_id='Load_artist_dim_table',
+        redshift_conn_id="redshift",
+        params={
+            'table': "dimArtists"
+        },
+        sql_delete=SqlQueries.artist_table_delete,
+        sql_create=SqlQueries.artist_table_create,
+        sql_insert=SqlQueries.artist_table_insert
+    )
+
+    load_time_dimension_table = LoadDimensionOperator(
+        task_id='Load_time_dim_table',
+        redshift_conn_id="redshift",
+        params={
+            'table': "dimTime"
+        },
+        sql_delete=SqlQueries.time_table_delete,
+        sql_create=SqlQueries.time_table_create,
+        sql_insert=SqlQueries.time_table_insert
+    )
+
+    run_quality_checks = DataQualityOperator(
+        task_id='Run_data_quality_checks',
+        redshift_conn_id="redshift",
+        params={
+            'tables': ["log_data", "song_data",  "factSongPlays", 
+                        "dimArtists", "dimTime", "dimUsers", "dimSongs"]
+            
+        }
+
+    )
+
+
+    start_operator >> stage_events_to_redshift
+
+    start_operator >> stage_songs_to_redshift
+
+    stage_events_to_redshift >> load_songplays_table
+    stage_songs_to_redshift >> load_songplays_table
+
+    
+
+    load_songplays_table >> load_song_dimension_table
+    load_songplays_table >> load_user_dimension_table
+    load_songplays_table >> load_artist_dimension_table
+    load_songplays_table >> load_time_dimension_table
+
+
+    load_song_dimension_table >> run_quality_checks
+    load_user_dimension_table >> run_quality_checks
+    load_artist_dimension_table >> run_quality_checks
+    load_time_dimension_table >> run_quality_checks
+
+    run_quality_checks >> end_operator
+
+
+
+
+final_project_dag = final_project()
--- plugins/helpers/sql_queries.py
+++ plugins/helpers/sql_queries.py
@@ -1,42 +1,207 @@
 class SqlQueries:
-    songplay_table_insert = ("""
-        SELECT
-                md5(events.sessionid || events.start_time) songplay_id,
-                events.start_time, 
-                events.userid, 
-                events.level, 
-                songs.song_id, 
-                songs.artist_id, 
-                events.sessionid, 
-                events.location, 
-                events.useragent
-                FROM (SELECT TIMESTAMP 'epoch' + ts/1000 * interval '1 second' AS start_time, *
-            FROM staging_events
-            WHERE page='NextSong') events
-            LEFT JOIN staging_songs songs
-            ON events.song = songs.title
-                AND events.artist = songs.artist_name
-                AND events.length = songs.duration
+    # CREATE TABLES
+    staging_events_table_create= ("""CREATE TABLE IF NOT EXISTS log_data (
+    artist VARCHAR,
+    auth VARCHAR,
+    firstName VARCHAR,
+    gender VARCHAR,
+    itemInSession INT,
+    lastName VARCHAR,
+    length FLOAT,
+    level VARCHAR,
+    location VARCHAR,
+    method VARCHAR,
+    page VARCHAR,
+    registration BIGINT,
+    sessionId INT,
+    song VARCHAR,
+    status INT,
+    ts BIGINT,
+    userAgent TEXT,
+    userId BIGINT);""")
+
+    staging_songs_table_create = ("""CREATE TABLE IF NOT EXISTS song_data (
+    artist_id   VARCHAR NOT NULL,
+    artist_name VARCHAR,
+    artist_location VARCHAR,
+    artist_latitude FLOAT,
+    artist_longitude FLOAT,
+    song_id VARCHAR NOT NULL,
+    title VARCHAR,
+    duration FLOAT,
+    year INT);""")
+
+    songplay_table_create = ("""
+    CREATE TABLE IF NOT EXISTS factSongPlays (
+        songplay_id  BIGINT IDENTITY(1,1) PRIMARY KEY,
+        start_time   BIGINT NOT NULL REFERENCES dimTime(start_time),
+        user_key     BIGINT NOT NULL REFERENCES dimUsers(user_key),
+        user_id      BIGINT,
+        level        VARCHAR NOT NULL,
+        song_id      VARCHAR NOT NULL REFERENCES dimSongs(song_id),
+        artist_id    VARCHAR NOT NULL REFERENCES dimArtists(artist_id),
+        session_id   INT NOT NULL,
+        location     VARCHAR,
+        user_agent   TEXT NOT NULL);""")
+
+    user_table_delete =("""
+    DROP TABLE IF EXISTS dimUsers;
     """)
+
+    user_table_create = ("""
+    CREATE TABLE IF NOT EXISTS dimUsers (
+        user_key   BIGINT IDENTITY(1,1) PRIMARY KEY,
+        user_id    BIGINT,
+        first_name VARCHAR,
+        last_name  VARCHAR,
+        gender     VARCHAR,
+        level      VARCHAR NOT NULL);""")
+
+    song_table_delete =("""
+    DROP TABLE IF EXISTS dimSongs;
+    """)
+
+    song_table_create = ("""
+    CREATE TABLE IF NOT EXISTS dimSongs (
+        song_id   VARCHAR NOT NULL PRIMARY KEY,
+        title     VARCHAR,
+        artist_id VARCHAR NOT NULL REFERENCES dimArtists(artist_id),
+        year      INT NOT NULL,
+        duration  INT NOT NULL);""")
+
+    artist_table_delete =("""
+    DROP TABLE IF EXISTS dimArtists;
+    """)
+
+    artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS dimArtists (
+        artist_id VARCHAR NOT NULL PRIMARY KEY,
+        name      VARCHAR,
+        location  VARCHAR,
+        latitude  FLOAT,
+        longitude FLOAT);""")
+
+    time_table_delete =("""
+    DROP TABLE IF EXISTS dimTime;
+    """)
+
+    time_table_create = ("""
+    CREATE TABLE IF NOT EXISTS dimTime (
+        start_time BIGINT NOT NULL PRIMARY KEY,
+        hour       INT NOT NULL,
+        day        INT NOT NULL,
+        week       INT NOT NULL,
+        month      INT NOT NULL,
+        year       INT NOT NULL,
+        weekday    INT NOT NULL);""")
+
+
+
+
+    # songplay_table_insert = ("""
+    #     INSERT INTO factSongPlays (song_id, artist_id,
+    #                         session_id,user_id, start_time,
+    #                         level, location, user_agent, user_key)
+    #     SELECT
+    #             md5(events.sessionid || events.start_time) songplay_id,
+    #             events.start_time, 
+    #             events.userid, 
+    #             events.level, 
+    #             songs.song_id, 
+    #             songs.artist_id, 
+    #             events.sessionid, 
+    #             events.location, 
+    #             events.useragent
+    #             FROM (SELECT TIMESTAMP 'epoch' + ts/1000 * interval '1 second' AS start_time, *
+    #         FROM log_data
+    #         WHERE page='NextSong') events
+    #         LEFT JOIN song_data songs
+    #         ON events.song = songs.title
+    #             AND events.artist = songs.artist_name
+    #             AND events.length = songs.duration
+    # """)
+
+    # songplay_table_insert = ("""
+    #     INSERT INTO factSongPlays (
+    #         start_time,
+    #         user_id,
+    #         level,
+    #         song_id,
+    #         artist_id,
+    #         session_id,
+    #         location,
+    #         user_agent
+    #     )
+    #     SELECT
+    #         CAST(events.userid AS BIGINT) AS user_id,
+    #         CAST(events.ts AS BIGINT) as start_time,
+    #         events.level,
+    #         songs.song_id,
+    #         songs.artist_id,
+    #         events.sessionid,
+    #         events.location,
+    #         events.useragent
+    #     FROM log_data events
+    #     LEFT JOIN song_data songs
+    #     ON events.song = songs.title
+    #         AND events.artist = songs.artist_name
+    #         AND events.length = songs.duration
+    #     WHERE events.page = 'NextSong'
+    # """)
+
+    songplay_table_insert = ("""
+        INSERT INTO factSongPlays (song_id, artist_id,
+                                    session_id,user_id, start_time,
+                                    level, location, user_agent, user_key)
+        SELECT sd.song_id AS song_id,
+                sd.artist_id AS artist_id,
+                ld.sessionId AS session_id,
+                ld.userId AS user_id,
+                ld.ts AS start_time,
+                ld.level AS level,
+                ld.location AS location,
+                ld.userAgent AS user_agent,
+                du.user_key AS user_key
+        FROM log_data ld
+        JOIN song_data sd ON
+        LOWER(REGEXP_REPLACE(ld.song, '[^a-zA-Z0-9]', '')) = LOWER(REGEXP_REPLACE(sd.title, '[^a-zA-Z0-9]', ''))
+        AND LOWER(REGEXP_REPLACE(ld.artist, '[^a-zA-Z0-9]', '')) = LOWER(REGEXP_REPLACE(sd.artist_name, '[^a-zA-Z0-9]', ''))
+        JOIN dimUsers du ON ld.userId = du.user_id;""")
 
     user_table_insert = ("""
-        SELECT distinct userid, firstname, lastname, gender, level
-        FROM staging_events
-        WHERE page='NextSong'
-    """)
+        INSERT INTO dimUsers (user_id, first_name, last_name, gender, level)
+        SELECT DISTINCT userId AS user_id,
+                        firstName AS first_name,
+                        lastName AS last_name,
+                        gender AS gender,
+                        level AS level
+        FROM log_data;""")
 
     song_table_insert = ("""
-        SELECT distinct song_id, title, artist_id, year, duration
-        FROM staging_songs
-    """)
+        INSERT INTO dimSongs (song_id,  title, artist_id, year, duration)
+        SELECT DISTINCT song_id AS song_id,
+                        title AS title,
+                        artist_id AS artist_id,
+                        year AS year,
+                        duration AS duration
+        FROM song_data;""")
 
     artist_table_insert = ("""
-        SELECT distinct artist_id, artist_name, artist_location, artist_latitude, artist_longitude
-        FROM staging_songs
-    """)
+        INSERT INTO dimArtists (artist_id, name, location, latitude, longitude)
+        SELECT DISTINCT artist_id AS artist_id,
+                        artist_name AS name,
+                        artist_location AS location,
+                        artist_latitude AS latitude,
+                        artist_longitude AS longitude
+        FROM song_data;""")
 
     time_table_insert = ("""
-        SELECT start_time, extract(hour from start_time), extract(day from start_time), extract(week from start_time), 
-               extract(month from start_time), extract(year from start_time), extract(dayofweek from start_time)
-        FROM songplays
-    """)
+        INSERT INTO dimTime (start_time, hour, day, week, month, year, weekday)
+        SELECT DISTINCT ts AS start_time,
+        EXTRACT(hour FROM TIMESTAMP 'epoch' + ts/1000 * INTERVAL '1 second')::INT AS hour,
+        EXTRACT(day FROM TIMESTAMP 'epoch' + ts/1000 * INTERVAL '1 second')::INT AS day,
+        EXTRACT(week FROM TIMESTAMP 'epoch' + ts/1000 * INTERVAL '1 second')::INT AS week,
+        EXTRACT(month FROM TIMESTAMP 'epoch' + ts/1000 * INTERVAL '1 second')::INT AS month,
+        EXTRACT(year FROM TIMESTAMP 'epoch' + ts/1000 * INTERVAL '1 second')::INT AS year,
+        EXTRACT(dow FROM TIMESTAMP 'epoch' + ts/1000 * INTERVAL '1 second')::INT AS weekday
+        FROM log_data;""")
